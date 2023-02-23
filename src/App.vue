f58eed00702467b4154b0258091c7fae9e717173
Azmi ŞAHİN <azmisahin@users.noreply.github.com>
2023-02-23 14:15:10 +0000

    create update

--- src/App.vue
+++ src/App.vue
@@ -220,9 +220,20 @@ export default {
       let response = await fetch(`${api}${endpoint}`)
       return await response.json();
     },
-    async create(endpoint) {
-
-      let response = await fetch(`${api}${endpoint}`)
+    async create(endpoint, data) {
+      let response = await fetch(`${api}${endpoint}`, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
       return await response.json();
     },
 
